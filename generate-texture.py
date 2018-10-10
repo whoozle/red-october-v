f97118dd04f7ce913101888342fb235e84ee3aa0
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-10-10 16:26:47 +0100

    added huffman implementation (no chip8 implementation)

--- generate-texture.py
+++ generate-texture.py
@@ -9,6 +9,7 @@ parser.add_argument('name', help='name')
 parser.add_argument('planes', type=int, help='planes (1/2)')
 parser.add_argument('tile-size', type=int, help='tile size (8/16)')
 parser.add_argument('--compress', '-c', action='store_true', help='compress bitmap with lz4 algorithm')
+parser.add_argument('--algorithm', '-A', type=str, help='select algo', default='lz4')
 parser.add_argument('--map0', type=int, help='map bg color to chip8 color')
 parser.add_argument('--map1', type=int, help='map palette color 1 to chip8 color')
 parser.add_argument('--map2', type=int, help='map palette color 2 to chip8 color')
@@ -54,9 +55,70 @@ def get_pixel(x, y, plane):
 	bit = 1 << plane
 	return 1 if value & bit else 0
 
-if args.compress:
-	from lz4.block import compress
+def huffman(data):
+	class Node(object):
+		def __init__(self, symbol, weight):
+			self.symbol, self.weight = symbol, weight
 
+		def __repr__(self):
+			return "Node(%d, %d)" %(self.symbol, self.weight)
+
+		def generate(self, prefix, result):
+			result(prefix, self.symbol)
+
+	class CompositeNode(object):
+		def __init__(self, zero, one):
+			self.zero, self.one = zero, one
+			self.weight = zero.weight + one.weight
+
+		def __repr__(self):
+			return "CompositeNode([%s, %s], %d)" %(repr(self.zero), repr(self.one), self.weight)
+
+		def generate(self, prefix, result):
+			self.zero.generate(prefix + '0', result)
+			self.one.generate(prefix + '1', result)
+
+	tokens = []
+	for b in data:
+		for i in xrange(4):
+			tokens.append((b >> (6 - i * 2)) & 3)
+	stats = { 0: 0, 1: 0, 2: 0, 3: 0 }
+	for b in tokens:
+		stats[b] += 1
+
+	nodes = []
+	for s, w in stats.iteritems():
+		nodes.append(Node(s, w))
+	while len(nodes) > 1:
+		nodes.sort(key = lambda x: x.weight)
+		z, o = nodes.pop(0), nodes.pop(0)
+		nodes.append(CompositeNode(z, o))
+
+	tree = {}
+	def result(bits, symbol):
+		print "# symbol %d -> %s" %(symbol, bits)
+		tree[symbol] = bits
+
+	nodes[0].generate('', result)
+	text = ''
+	for t in tokens:
+		text += tree[t]
+
+	n = len(text)
+	r = n % 8
+	if r:
+		for i in xrange(8 - r):
+			text += '0'
+	n = len(text)
+
+	data = bytearray()
+	for i in xrange(0, n, 8):
+		data.append(int(text[i: i + 8], 2))
+
+	return data
+
+
+if args.compress:
 	print label("cdata")
 	packed_data = bytearray()
 	for ty in xrange(0, ny):
@@ -70,7 +132,12 @@ if args.compress:
 						for bit in xrange(0, 8):
 							byte |= get_pixel(basex + x * 8 + bit, basey + y, plane) << (7 - bit)
 						packed_data.append(byte)
-	compressed_data = compress(packed_data, return_bytearray = True, mode='high_compression', compression=12, store_size=False)
+	if args.algorithm == "lz4":
+		from lz4.block import compress
+		compressed_data = compress(packed_data, return_bytearray = True, mode='high_compression', compression=12, store_size=False)
+	elif args.algorithm == "huffman":
+		compressed_data = huffman(packed_data)
+
 	print "#compressed size: %d of %d\n" %(len(compressed_data), len(packed_data))
 	print " ".join(map(lambda x: "0x%02x" %x, compressed_data))
 
